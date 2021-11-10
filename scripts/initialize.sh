fdbf7692e39adaaacaf17b97d60b3076db4a73ef
Jin SungJun <wlstjdwns115@gmail.com>
2021-11-10 16:16:27 +0900

    FIX: fix scripts destination to Maplody and index.js

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/ubuntu/im-sprint-practice-deploy/server
+cd /home/ubuntu/Maplody/server
 npm install
 npm install pm2@latest -g
 sudo apt-get update
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /home/ubuntu/im-sprint-practice-deploy/server
-authbind --deep pm2 start app.js
+cd /home/ubuntu/Maplody/server
+authbind --deep pm2 start index.js
