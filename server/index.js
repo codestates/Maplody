299ea1111b1094a162a5733be840d876fac73c84
anniemon <seojung19@gmail.com>
2021-11-11 14:24:23 +0900

    ADD: server pipeline scripts, index.js, db connection.js

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const db = require('./db/connection');
+
+const app = express();
+app.use(express.json());
+const port = 80;
+
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  }),
+);
+
+app.get('/', (req, res) => {
+  res.status(201).send('Hello World');
+});
+
+server.keepAliveTimeout = 65000;
+server.headersTimeout = 66000;
+
+app.listen(port, () => {
+  console.log(`서버가 ${port}번에서 작동중입니다.`);
+});
