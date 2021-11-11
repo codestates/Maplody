eca71c754f82629e75daa7da1392ddfdbae3746e
anniemon <seojung19@gmail.com>
2021-11-11 23:09:48 +0900

    MERGE: upstream dev with google api

--- client/src/App.js
+++ client/src/App.js
@@ -8,11 +8,6 @@ import Landing from './pages/Landing';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <header className="App-header">
-        <p>hello world SUDO_HIRED!</p>
-      </header>
-=======
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Landing />} />
@@ -20,7 +15,6 @@ function App() {
           <Route path="/main" element={<Main />} />
         </Routes>
       </BrowserRouter>
->>>>>>> 83b67355a6d1c58f79a9c4a808422c15cf939da7
     </div>
   );
 }
