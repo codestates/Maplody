d4e2e0f184518d9ec152add140e4ff779da47d7b
Hendrix1995 <hgud55@naver.com>
2021-11-12 13:14:03 +0900

    ADD: client componets Maps add Marker

--- client/src/components/Maps.js
+++ client/src/components/Maps.js
@@ -1,19 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import { useState } from 'react';
-import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps';
-
-const map = () => {
-  return (
-    <GoogleMap
-      defaultZoom={13}
-      defaultCenter={{ lat: 37.51249519205713, lng: 126.99480974427608 }}
-      options={{ disableDefaultUI: true }}
-    />
-  );
-};
-
-const WrappedMap = withScriptjs(withGoogleMap(map));
+import { GoogleMap, withScriptjs, withGoogleMap, Marker } from 'react-google-maps';
 
 const MapContainer = styled.div`
   display: inline;
@@ -22,9 +9,28 @@ const MapContainer = styled.div`
   z-index: -100;
 `;
 
-const Maps = () => {
-  const [places, setPlaces] = useState([]);
+const Map = () => {
+  const [target, setTarget] = useState({ lat: null, lng: null });
 
+  const addMarkerHandler = (e) => {
+    setTarget({ lat: e.latLng.lat(), lng: e.latLng.lng() });
+    console.log({ lat: e.latLng.lat(), lng: e.latLng.lng() });
+  };
+
+  return (
+    <GoogleMap
+      defaultZoom={13}
+      defaultCenter={{ lat: 37.51249519205713, lng: 126.99480974427608 }}
+      options={{ disableDefaultUI: true }}
+      onClick={addMarkerHandler}>
+      <Marker position={target} />;{/* 마커를 찍으면 저장하고 그 이후 그 갯수마다 다 불러온다. */}
+    </GoogleMap>
+  );
+};
+
+const WrappedMap = withScriptjs(withGoogleMap(Map));
+
+const Maps = () => {
   return (
     <MapContainer>
       <WrappedMap
