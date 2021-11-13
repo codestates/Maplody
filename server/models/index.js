af0a6542fef4f25bac8a17e3bc2726cdb6b2df3c
anniemon <seojung19@gmail.com>
2021-11-13 11:56:11 +0900

    VERIFY: config env_variable working

--- server/models/index.js
+++ server/models/index.js
@@ -10,6 +10,7 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
+  console.log('env???');
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
