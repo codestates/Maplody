7e0806e67e5441bd8498c7e55a362a27466adb26
anniemon <seojung19@gmail.com>
2021-11-13 11:44:08 +0900

    UPDATE: rds port in config.js

--- server/config/config.js
+++ server/config/config.js
@@ -14,6 +14,7 @@ module.exports = {
     password: process.env.DATABASE_PASSWORD_RDS,
     database: process.env.DATABASE_NAME_RDS,
     host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
     dialect: 'mysql',
   },
 };
