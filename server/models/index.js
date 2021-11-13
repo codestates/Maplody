abeb5f2f218871e5ce65cc69d7f61c6ab62cc8c3
anniemon <seojung19@gmail.com>
2021-11-13 14:53:34 +0900

    FIX: sequelize config env

--- server/models/index.js
+++ server/models/index.js
@@ -8,13 +8,7 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  console.log('env???');
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+let sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 fs.readdirSync(__dirname)
   .filter((file) => {
