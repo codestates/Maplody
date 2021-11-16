87daf0c1911aae0ac553ce95ac61b61a5ac05663
Hendrix1995 <hgud55@naver.com>
2021-11-16 20:58:18 +0900

    ADD: client page add Loading

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { useState } from 'react';
-import styled from 'styled-components';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import styled, { keyframes } from 'styled-components';
 
 import Button from '../components/Button';
 import LoginModal from '../components/LoginModal';
 import Footer from '../components/Footer';
-import axios from 'axios';
+import Loading from './Loading';
 require('dotenv').config();
 
 axios.defaults.withCredentials = true;
@@ -37,7 +38,10 @@ const About = styled.div`
   background-color: white;
 `;
 
-const MapContainer = styled.div``;
+const MapContainer = styled.div`
+  width: fit-content;
+  height: fit-content;
+`;
 
 const LandingMap = styled.img`
   position: absolute;
@@ -54,42 +58,54 @@ const LandingMap = styled.img`
 const Landing = ({ accessToken, setAccessToken, setIsLogin }) => {
   const [loginOpen, setLoginOpen] = useState(false);
 
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(false);
+  }, []);
+
   const openModalHandler = () => {
     setLoginOpen(!loginOpen);
   };
 
   return (
-    <LandingContainer>
-      <AboutContainer>
-        <Logo src={require('../img/Maplody_Logo.png').default} />
-        <About>
-          <p>"기억하고 싶은 순간의 들리던 음악을 소중한 추억으로 간직하세요"</p>
-          <br />
-          <p>누구나 한 순간 기억하고 싶은 때가 있습니다.</p>
-          <p>그 순간을 함께한 소중한 인연과</p>
-          <p>그때 들려오던 음악을 저장하고 추억을 언제나 꺼내 보세요.</p>
-          <br />
-          <p>언젠가 그때 그 노래가 들려오면...</p>
-          <p>다시 그곳에 방문하면...</p>
-          <br />
-          당신을 다시 그곳으로 돌려 놓을
-          <p>"Maplody 하세요"</p>
-        </About>
-        <Button text="시작하기" onClick={openModalHandler} />
-        {loginOpen ? (
-          <LoginModal
-            accessToken={accessToken}
-            setAccessToken={setAccessToken}
-            openModalHandler={openModalHandler}
-            setIsLogin={setIsLogin}
-          />
-        ) : null}
-      </AboutContainer>
-      <MapContainer>
-        <LandingMap src={require('../img/서울.jpeg').default} />
-      </MapContainer>
-      <Footer />
-    </LandingContainer>
+    <>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <LandingContainer>
+          <AboutContainer>
+            <Logo src={require('../img/Maplody_Logo.png').default} />
+            <About>
+              <p>"기억하고 싶은 순간의 들리던 음악을 소중한 추억으로 간직하세요"</p>
+              <br />
+              <p>누구나 한 순간 기억하고 싶은 때가 있습니다.</p>
+              <p>그 순간을 함께한 소중한 인연과</p>
+              <p>그때 들려오던 음악을 저장하고 추억을 언제나 꺼내 보세요.</p>
+              <br />
+              <p>언젠가 그때 그 노래가 들려오면...</p>
+              <p>다시 그곳에 방문하면...</p>
+              <br />
+              당신을 다시 그곳으로 돌려 놓을
+              <p>"Maplody 하세요"</p>
+            </About>
+            <Button text="시작하기" onClick={openModalHandler} />
+            {loginOpen ? (
+              <LoginModal
+                accessToken={accessToken}
+                setAccessToken={setAccessToken}
+                openModalHandler={openModalHandler}
+                setIsLogin={setIsLogin}
+              />
+            ) : null}
+          </AboutContainer>
+          <MapContainer>
+            <LandingMap src={require('../img/서울.jpeg').default} />
+          </MapContainer>
+          <Footer />
+        </LandingContainer>
+      )}
+    </>
   );
 };
 export default Landing;
