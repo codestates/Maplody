1f26ed1dda99cd9b7801e0c819b99fb1b676cf9f
anniemon <seojung19@gmail.com>
2022-01-14 00:46:22 +0900

    REFACTOR: use @react-google-maps/api instead of react-google-maps library remove marker when post deleted

--- client/src/components/Maps.js
+++ client/src/components/Maps.js
@@ -1,26 +1,55 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import styled from 'styled-components';
-import { GoogleMap, withScriptjs, withGoogleMap, Marker, InfoWindow } from 'react-google-maps';
+import { GoogleMap, Marker, InfoWindow, useLoadScript } from '@react-google-maps/api';
 import { useNavigate } from 'react-router';
 import Post from './Post';
 import NewPostModal from './NewPostModal';
+import Loading from '../pages/Loading';
 
-const MapContainer = styled.div`
-  display: inline;
-  width: 100%;
-  height: 100%;
-  z-index: -100;
-`;
+const libraries = ['places'];
+const mapContainerStyle = {
+  width: '100vw',
+  height: '100vh',
+};
 
-const Map = ({ accessToken, issueTokens }) => {
+const options = {
+  disableDefaultUI: true,
+  zoomControl: true,
+};
+
+const center = {
+  lat: 37.51249519205713,
+  lng: 126.99480974427608,
+};
+
+const Maps = ({ accessToken, issueTokens }) => {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_KEY,
+    libraries,
+  });
+  const [posts, setPosts] = useState([]);
   const [target, setTarget] = useState({ lat: null, lng: null });
   const [selected, setSelected] = useState(null);
   const [isOpenNewPostModal, setIsOpenNewPostModal] = useState(false);
   const [getAddress, setGetAddress] = useState(null);
-  const [post, setPost] = useState([]);
   const [markerVisible, setMarkerVisible] = useState(true);
-  const navigate = useNavigate();
+  const [isDeleted, setIsDeleted] = useState(false);
+
+  const getPostHandler = () => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/post`, {
+        headers: { authorization: `Bearer ${accessToken}` },
+        withCredentials: true,
+      })
+      .then((res) => {
+        setPosts(res.data.data);
+        setIsDeleted(false);
+        console.log('res.data', res.data.data);
+      })
+      .catch((err) => {
+        issueTokens();
+      });
+  };
 
   const paramsAddress = {
     latlng: `${target.lat},${target.lng}`,
@@ -39,114 +68,98 @@ const Map = ({ accessToken, issueTokens }) => {
     return;
   };
 
-  const addMarkerHandler = (e) => {
-    setTarget({ lat: e.latLng.lat(), lng: e.latLng.lng() });
-    markerAddressHandler();
-  };
+  useEffect(() => {
+    getPostHandler();
+    console.log('useEffect');
+  }, [isOpenNewPostModal, isDeleted]);
+
+  if (loadError) {
+    return 'Error loading maps';
+  }
+  if (!isLoaded) {
+    return <Loading />;
+  }
 
   const openNewPostModalHandler = () => {
     setIsOpenNewPostModal(!isOpenNewPostModal);
   };
 
-  const getPostHandler = () => {
-    axios
-      .get(`${process.env.REACT_APP_API_URL}/post`, {
-        headers: { authorization: `Bearer ${accessToken}` },
-        withCredentials: true,
-      })
-      .then((res) => {
-        setPost(res.data.data);
-      })
-      .catch((err) => {
-        issueTokens();
-      });
-  };
-
-  useEffect(() => {
-    getPostHandler();
-    markerAddressHandler();
-  }, [isOpenNewPostModal]);
-
   return (
-    <GoogleMap
-      defaultZoom={13}
-      defaultCenter={{ lat: 37.51249519205713, lng: 126.99480974427608 }}
-      options={{ disableDefaultUI: true }}
-      onClick={(e) => {
-        addMarkerHandler(e);
-        setMarkerVisible(true);
-      }}>
-      <Marker onClick={openNewPostModalHandler} animation={2} position={target} visible={markerVisible}>
-        {isOpenNewPostModal ? (
-          <InfoWindow zIndex={998}>
-            <NewPostModal
-              issueTokens={issueTokens}
-              setMarkerVisible={setMarkerVisible}
-              post={post}
-              setPost={setPost}
-              target={target}
-              getAddress={getAddress}
-              openNewPostModalHandler={openNewPostModalHandler}
-              navigate={navigate}
-            />
-          </InfoWindow>
-        ) : null}
-      </Marker>
-
-      {post.map((el) => (
-        <Marker
-          key={el.id}
-          position={{
-            lat: Number(el.lat),
-            lng: Number(el.lng),
-          }}
-          place={el.place}
-          music={el.music}
-          onClick={() => {
-            setSelected(el);
-          }}
-          icon={{ url: require('../img/music-notes.png').default }}>
-          {selected && selected.id === el.id && (
-            <InfoWindow
-              onCloseClick={() => {
-                setSelected(null);
-              }}>
-              <Post
-                key={el.id}
-                id={el.id}
-                getAddress={el.getAddress}
-                musicArtist={el.musicArtist}
-                musicTitle={el.musicTitle}
-                createdAt={el.createdAt}
-                url={el.url}
-                storyBoard={el.storyBoard}
-                setSelected={setSelected}
-                issueTokens={issueTokens}
-                navigate={navigate}
-              />
-            </InfoWindow>
-          )}
-        </Marker>
-      ))}
-    </GoogleMap>
-  );
-};
-
-const WrappedMap = withScriptjs(withGoogleMap(Map));
-
-const Maps = ({ accessToken, issueTokens }) => {
-  return (
-    <MapContainer>
-      <WrappedMap
+    <div>
+      <GoogleMap
         accessToken={accessToken}
         issueTokens={issueTokens}
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&
-        libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
-        loadingElement={<div style={{ height: '100%' }} />}
-        containerElement={<div style={{ height: '100%' }} />}
-        mapElement={<div style={{ height: '100%' }} />}
-      />
-    </MapContainer>
+        mapContainerStyle={mapContainerStyle}
+        zoom={13}
+        center={center}
+        options={options}
+        onClick={(e) => {
+          setTarget({ lat: e.latLng.lat(), lng: e.latLng.lng() });
+          markerAddressHandler();
+          setMarkerVisible(true);
+        }}>
+        <Marker
+          onClick={() => {
+            openNewPostModalHandler();
+            markerAddressHandler();
+          }}
+          animation={2}
+          position={target}
+          visible={markerVisible}>
+          {isOpenNewPostModal ? (
+            <InfoWindow zIndex={998}>
+              <NewPostModal
+                issueTokens={issueTokens}
+                setMarkerVisible={setMarkerVisible}
+                posts={posts}
+                setPosts={setPosts}
+                target={target}
+                getAddress={getAddress}
+                openNewPostModalHandler={openNewPostModalHandler}
+              />
+            </InfoWindow>
+          ) : null}
+        </Marker>
+
+        {posts.map((el) => (
+          <Marker
+            key={el.id}
+            position={{
+              lat: Number(el.lat),
+              lng: Number(el.lng),
+            }}
+            onClick={() => {
+              setSelected(el);
+            }}
+            icon={{ url: require('../img/music-notes.png').default }}>
+            {selected && selected.id === el.id && (
+              <InfoWindow
+                key={el.id}
+                onCloseClick={() => {
+                  setSelected(null);
+                }}>
+                <Post
+                  key={el.id}
+                  id={el.id}
+                  getAddress={el.getAddress}
+                  musicArtist={el.musicArtist}
+                  musicTitle={el.musicTitle}
+                  createdAt={el.createdAt}
+                  url={el.url}
+                  storyBoard={el.storyBoard}
+                  setSelected={setSelected}
+                  issueTokens={issueTokens}
+                  setPosts={setPosts}
+                  getPostHandler={getPostHandler}
+                  posts={posts}
+                  setIsDeleted={setIsDeleted}
+                />
+              </InfoWindow>
+            )}
+          </Marker>
+        ))}
+      </GoogleMap>
+    </div>
   );
 };
 
